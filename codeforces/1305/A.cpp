e9b810fd354b3266391abc1a1bc3233773706f2f
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-03-03 21:51:00 +0600

    Add solution for problem `A - Kuroni and the Gifts` Link: https://codeforces.com/contest/1305/problem/A Tags: brute force, constructive algorithms, greedy, sortings, *800 Ref: https://codeforces.com/contest/1305/submission/72339451

--- codeforces/1305/A.cpp
+++ codeforces/1305/A.cpp
@@ -1,33 +1,58 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-  int tt;
-  cin >> tt;
-  while (tt--) {
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-      cin >> a[i];
+void task()
+{
+    int t, n, pos_a, pos_b, i, j;
+    int a[1000], b[1000];
+
+    cin>>t;
+    while(t--){
+        cin>>n;
+
+        for(i=0; i<n; i++){
+            cin>>a[i];
+        }
+        for(i=0; i<n; i++){
+            cin>>b[i];
+        }
+
+        for(i=1; i<n; i++){
+            pos_a = i;
+            pos_b = i;
+            for(j=i-1; j>=0; j--){
+                if(a[pos_a]>a[pos_a-1]) break;
+
+                if(a[pos_a]<a[pos_a-1]){
+                    swap(a[pos_a], a[pos_a-1]);
+                }
+                pos_a--;
+            }
+
+            for(j=i-1; j>=0; j--){
+                if(b[pos_b]>b[pos_b-1]) break;
+
+                if(b[pos_b]<b[pos_b-1]){
+                    swap(b[pos_b], b[pos_b-1]);
+                }
+                pos_b--;
+            }
+        }
+
+        for(i=0; i<n; i++){
+            cout<<a[i]<<' ';
+        }
+        cout<<endl;
+        for(i=0; i<n; i++){
+            cout<<b[i]<<' ';
+        }
+        cout<<endl;
     }
-    vector<int> b(n);
-    for (int i = 0; i < n; i++) {
-      cin >> b[i];
-    }
-    sort(a.begin(), a.end());
-    sort(b.begin(), b.end());
-    for (int i = 0; i < n; i++) {
-      if (i > 0) cout << " ";
-      cout << a[i];
-    }
-    cout << '\n';
-    for (int i = 0; i < n; i++) {
-      if (i > 0) cout << " ";
-      cout << b[i];
-    }
-    cout << '\n';
-  }
-  return 0;
+}
+
+int main()
+{
+    task();
+
+    return 0;
 }
