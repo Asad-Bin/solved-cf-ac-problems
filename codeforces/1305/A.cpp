3e7d9b1faa6c2fdc56315a096d4ba2ce3bb847b7
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-03-05 12:37:00 +0600

    Add solution for problem `A - Kuroni and the Gifts` Link: https://codeforces.com/contest/1305/problem/A Tags: brute force, constructive algorithms, greedy, sortings, *800 Ref: https://codeforces.com/contest/1305/submission/72490825

--- codeforces/1305/A.cpp
+++ codeforces/1305/A.cpp
@@ -2,35 +2,32 @@
 
 using namespace std;
 
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        //cout<<t;
-        int n;
-        cin >> n;
-        vector<int> a(n);
-        for(int i = 0; i < n; i++){
-            cin >> a[i];
-        }
-        vector<int> b(n);
-        for(int i = 0; i < n; i++){
-            cin >> b[i];
-        }
-        sort(a.begin(), a.end());
-        sort(b.begin(), b.end());
-
-        for(int i=0; i<n; i++){
-            if(i>0) cout << " ";
-            cout << a[i];
-        }
-        cout << '\n';
-        for(int i=0; i<n; i++){
-            if(i>0) cout << " ";
-            cout << b[i];
-        }
-        cout << '\n';
+int main() {
+  int tt;
+  cin >> tt;
+  while (tt--) {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+      cin >> a[i];
     }
-
-    return 0;
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+      cin >> b[i];
+    }
+    sort(a.begin(), a.end());
+    sort(b.begin(), b.end());
+    for (int i = 0; i < n; i++) {
+      if (i > 0) cout << " ";
+      cout << a[i];
+    }
+    cout << '\n';
+    for (int i = 0; i < n; i++) {
+      if (i > 0) cout << " ";
+      cout << b[i];
+    }
+    cout << '\n';
+  }
+  return 0;
 }
