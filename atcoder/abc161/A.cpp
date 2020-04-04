853ebd5cb3970180bc833bb43f6feb023fcd23f7
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-04-04 18:04:00 +0600

    Add solution for problem `A - ABC Swap` Link: https://atcoder.jp/contests/abc161/tasks/abc161_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc161/submissions/11495864

---
+++ atcoder/abc161/A.cpp
@@ -0,0 +1,27 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 04 04 2020
+
+#include <stdio.h>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+void task()
+{
+    int x, y, z;
+    cin>>x>>y>>z;
+
+    cout<<z<<' '<<x<<' '<<y<<endl;
+}
+
+int main()
+{
+    task();
+
+    return 0;
+}
