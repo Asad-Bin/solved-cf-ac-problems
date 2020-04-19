e6c5c2f6385b90a880381d27782615b49c374569
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-04-19 18:19:00 +0600

    Add solution for problem `B - Homework` Link: https://atcoder.jp/contests/abc163/tasks/abc163_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc163/submissions/12117452

---
+++ atcoder/abc163/B.cpp
@@ -0,0 +1,45 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 19 04 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define    pi    acos(-1.0)
+
+void task()
+{
+    int n, m, x, ans=0;
+
+    cin >> n >> m;
+
+    vector<int> ara(m);
+
+    for(int K=0; K<m; K++){
+        cin >> x;
+        ans+=x;
+    }
+
+    if(ans>n) cout << "-1" << "\n";
+    else cout << n-ans << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    //clock_t tStart = clock();
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    //printf("Time taken: %.3fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    return 0;
+}
