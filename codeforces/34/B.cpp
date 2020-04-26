1d7332c20eca4ed603eb796f6e0b1e61fbe1bc91
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-04-27 02:20:00 +0600

    Add solution for problem `B - Sale` Link: https://codeforces.com/contest/34/problem/B Tags: greedy, sortings, *900 Ref: https://codeforces.com/contest/34/submission/78234225

---
+++ codeforces/34/B.cpp
@@ -0,0 +1,66 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 27 04 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+void task()
+{
+    int n, m, ans=0;
+
+    cin >> n >> m;
+    vector<int> ara(n);
+
+    for(int K=0; K<n; K++) cin >> ara[K];
+
+    sort(ara.begin(), ara.end());
+
+    for(int K=0; K<m; K++){
+        if(ara[K]<0) ans -= ara[K];
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    //clock_t tStart = clock();
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    //printf("Time taken: %.3fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    return 0;
+}
