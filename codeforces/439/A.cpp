ed749b0200e13e3e5b7a9b7d7014b99a295724fe
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-04-27 02:50:00 +0600

    Add solution for problem `A - Devu, the Singer and Churu, the Joker` Link: https://codeforces.com/contest/439/problem/A Tags: greedy, implementation, *900 Ref: https://codeforces.com/contest/439/submission/78236408

---
+++ codeforces/439/A.cpp
@@ -0,0 +1,67 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 27 04 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+void task()
+{
+    ll n, d, x, time=0, ans;
+
+    cin >> n >> d;
+
+    for(ll K=1; K<=n; K++){
+        cin >> x;
+
+        if(K!=n) time+=(x+10);
+        else time+=x;
+    }
+
+    ans = 2*(n-1);
+
+    if(time>d) cout << "-1\n";
+    else cout << ans+(ll)(d-time)/5 << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    //clock_t tStart = clock();
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    //printf("Time taken: %.3fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    return 0;
+}
