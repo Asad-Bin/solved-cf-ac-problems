928822b6e59fc9a634b9e5ee43d0184e066ae328
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-04-30 01:59:00 +0600

    Add solution for problem `B - Lovely Palindromes` Link: https://codeforces.com/contest/688/problem/B Tags: constructive algorithms, math, *1000 Ref: https://codeforces.com/contest/688/submission/78513976

---
+++ codeforces/688/B.cpp
@@ -0,0 +1,60 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 30 04 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+void task()
+{
+    string a;
+
+    cin >> a;
+
+    cout << a;
+    int len = a.length();
+    for(int K=len-1; K>=0; K--) cout << a[K];
+    cout << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //clock_t tStart = clock();
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    //printf("Time taken: %.3fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    return 0;
+}
