22a08ff0cbb62238495babf615486832e9746b60
Asad Bin Saber <mdasad.binsaber@gmail.com>
2020-05-02 18:18:00 +0600

    Add solution for problem `D - Floor Function` Link: https://atcoder.jp/contests/abc165/tasks/abc165_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc165/submissions/12593426

---
+++ atcoder/abc165/D.cpp
@@ -0,0 +1,62 @@
+//...Bismillahir Rahmanir Rahim...
+// Code by    Asad Bin Saber
+// 02 05 2020
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<int, pii> piii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+
+// constants...
+const double PI = acos(-1);
+
+// defines...
+#define MP make_pair
+#define PB push_back
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*(b/gcd(a,b))
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+
+void task()
+{
+    ll a, b, n;
+
+    cin >> a >> b >> n;
+
+    ll mid = min(b-1, n);
+
+    ll ans = (ll)(a*mid/b) - a*(ll)(mid/b);
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    //clock_t tStart = clock();
+
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    task();
+
+    //printf("Time taken: %.3fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
+
+    return 0;
+}
+
