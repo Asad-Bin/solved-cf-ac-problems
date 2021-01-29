c1e8d08cd77898a82338ab684cb0b84662c0d025
Asad Bin Saber <mdasad.binsaber@gmail.com>
2021-01-29 21:51:00 +0600

    Add solution for problem `B - Inflation` Link: https://codeforces.com/contest/1476/problem/B Tags: binary search, brute force, greedy, math, *1300 Ref: https://codeforces.com/contest/1476/submission/105915396

---
+++ codeforces/1476/B.cpp
@@ -0,0 +1,83 @@
+//...Bismillahir Rahmanir Rahim...
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// typedefs...
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pll;
+ 
+// constants...
+const double PI = acos(-1);
+const int MOD = 1e9+7; // 998244353;
+const int MXS = 2e5+5;
+const int MXI = 1e9+5;
+const ll MXL = 1e18+5;
+ 
+// defines...
+#define MP make_pair
+#define PB push_back
+#define fi first
+#define se second
+#define sz(x) (int)x.size()
+#define all(x) begin(x), end(x)
+ 
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+
+// structs...
+struct hash_pair { 
+    template <class T1, class T2> 
+    size_t operator()(const pair<T1, T2>& p) const{ auto hash1 = hash<T1>{}(p.first); auto hash2 = hash<T2>{}(p.second); return hash1 ^ hash2;} 
+};
+
+// functions...
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll comb(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K; ans /= L;} return ans;}
+
+
+void task()
+{
+	// Code here.. ..
+	
+	ll t, n, k, ara[105], sum[105];
+	
+	cin >> t;
+	while(t--){
+		cin >> n >> k;
+		
+		for(int K=0; K<n; K++){
+			cin >> ara[K];
+			sum[K] = ara[K] + (K>0?sum[K-1]:0LL);
+		}
+		
+		ll ans = 0;
+		for(int K=1; K<n; K++){
+			if(ara[K]*100LL > (sum[K-1]+ans)*k){
+				ans += (ara[K]*100LL - (ans+sum[K-1])*k + k - 1)/k;
+			}
+		}
+		
+		cout << ans << "\n";
+	}
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+	task();
+    return 0;
+}
