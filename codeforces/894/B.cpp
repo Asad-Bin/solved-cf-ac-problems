70c62eb92c0be6af2d015f992ee2009cf1c8581a
Asad Bin Saber <mdasad.binsaber@gmail.com>
2021-05-03 02:35:00 +0600

    Add solution for problem `B - Ralph And His Magic Field` Link: https://codeforces.com/contest/894/problem/B Tags: combinatorics, constructive algorithms, math, number theory, *1800 Ref: https://codeforces.com/contest/894/submission/114968530

---
+++ codeforces/894/B.cpp
@@ -0,0 +1,118 @@
+//...Bismillahir Rahmanir Rahim...
+// ***Asad Bin Saber***
+ 
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// typedefs...
+typedef long long            ll;
+typedef unsigned long long   ull;
+typedef pair<int, int>       pii;
+typedef vector<int>          vi;
+typedef vector<ll>           vl;
+typedef pair<ll, ll>         pll;
+ 
+// constants...
+//const double PI = acos(-1);
+const int    mod = 1e9+7; // 998244353;
+const int    MXI = 1e9+5;
+const ll     MXL = 1e18+5;
+ 
+// defines...
+#define MP             make_pair
+#define PB             push_back
+#define fi             first
+#define se             second
+#define sz(x)          (int)x.size()
+#define all(x)         begin(x), end(x)
+#define si(a)          scanf("%d", &a)
+#define sii(a, b)      scanf("%d%d", &a, &b)
+#define siii(a, b, c)  scanf("%d%d%d", &a, &b, &c)
+#define precise_impact cout<<setprecision(6)<<fixed;
+ 
+// chess moves...
+// int dx[] = {+0, +0, -1, +1}; ///Up-down, Left-Right
+// int dy[] = {+1, -1, +0, +0};
+// int dx[] = {+0,+0,+1,-1,-1,+1,-1,+1}; ///King's Move
+// int dy[] = {-1,+1,+0,+0,+1,+1,-1,-1};
+// int dx[] = {-2, -2, -1, -1,  1,  1,  2,  2}; ///Knight's Move
+// int dy[] = {-1,  1, -2,  2, -2,  2, -1,  1};
+ 
+// structs...
+struct hash_pair{ template <class T1, class T2> size_t operator()(const pair<T1, T2>& p) const{ auto hash1 = hash<T1>{}(p.first); auto hash2 = hash<T2>{}(p.second); return hash1 ^ hash2;} };
+ 
+// functions...
+ll gcd(ll a, ll b){ while (b){ a %= b; swap(a, b);} return a;}
+ll lcm(ll a, ll b){ return (a/gcd(a, b)*b);}
+ll bigmod(ll a, ll p, ll M)
+{
+	// If 'M' is a prime number, then we can speed up it just by calculating "a^(p%(M-1))";
+	p %= (M-1);
+	
+	ll result = 1LL;
+	while(p>0LL){
+		if(p&1LL) result = result * a % M;
+		a = a * a % M;
+		p>>=1LL;
+	}
+	return result;
+}
+ll inverse(ll a, ll M){
+	if(gcd(a, M)==1) return bigmod(a, M-2, M) % M; // (ax)%M = 1
+	return 1;
+}
+ll ncr(ll a, ll b){ ll x = max(a-b, b), ans=1; for(ll K=a, L=1; K>=x+1; K--, L++){ ans = ans * K % mod; ans = ans * inverse(L, mod) % mod;} return ans;}
+ll egcd(ll a,ll b,ll &x,ll &y){
+	if(a==0){
+		x=0; y=1;
+		return b;
+	}
+	
+	ll x1,y1;
+	ll d=egcd(b%a,a,x1,y1);
+	x=y1-(b/a)*x1; y=x1;
+	return d;
+}
+void pbin(int n)
+{
+	vector<int> vv;
+	while(n){
+		vv.PB(n%2);
+		n >>= 1;
+	}
+	for(int K=sz(vv); K<=5; K++) vv.PB(0);
+	reverse(all(vv));
+	for(auto x:vv) cout << x;
+	cout << "\n";
+}
+
+
+void task()
+{
+	// Code here.. ..
+	
+	ll n, m, k;
+	
+	cin >> n >> m >> k;
+	
+	if(k==-1 && (m+n)&1LL){
+		cout << 0 << "\n";
+		return;
+	}
+	if(k==1 && !(m+n)&1LL){
+		cout << 0 << "\n";
+		return;
+	}
+	
+	cout << bigmod(bigmod(2LL, (n-1), mod), (m-1), mod) << "\n";
+}
+ 
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	task();
+	return 0;
+}
