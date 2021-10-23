670ae7977b8b16f07008a9ccc7258aa3dbd50ba0
Asad Bin Saber <mdasad.binsaber@gmail.com>
2021-10-23 18:09:00 +0600

    Add solution for problem `B - Mongeness` Link: https://atcoder.jp/contests/abc224/tasks/abc224_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc224/submissions/26761039

---
+++ atcoder/abc224/B.cpp
@@ -0,0 +1,134 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+#define pii pair<int, int>
+#define vi vector<int>
+#define vl vector<long long>
+#define PB push_back
+#define sz(x) (int)x.size()
+#define fi first
+#define se second
+#define si(a) scanf("%d", &a)
+#define sii(a, b) scanf("%d%d", &a, &b)
+#define siii(a, b, c) scanf("%d%d%d", &a, &b, &c)
+#define sl(a) scanf("%lld", &a)
+#define sll(a, b) scanf("%lld%lld", &a, &b)
+#define slll(a, b, c) scanf("%lld%lld%lld", &a, &b, &c)
+//#define FASTIO ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef LOCAL
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+
+const int MX = 50;
+int ara[MX+5][MX+5];
+int main()
+{
+	#ifdef LOCAL
+        freopen("in.txt","r",stdin);
+        //freopen("out.txt","w",stdout);
+    #endif
+	
+    int n, m;
+    
+    sii(n, m);
+    
+    for(int K=0; K<n; K++){
+        for(int L = 0; L < m; L++) si(ara[K][L]);
+    }
+    
+    bool ok = 1;
+    
+    for(int K = 0; K < n; K++){
+        for(int L = 0; L < m; L++){
+            for(int i = 0; i < K; i++){
+                for(int j = 0;  j < L; j++){
+                    if(ara[K][L] + ara[i][j] > ara[i][L] + ara[K][j]) ok = 0;
+                }
+            }
+        }
+    }
+    
+    if(ok) printf("Yes\n");
+    else printf("No\n");
+	
+    #ifdef LOCAL
+        cerr << "Time elapsed: " << 1.0 * clock() / CLOCKS_PER_SEC << " s.\n";
+    #endif
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
