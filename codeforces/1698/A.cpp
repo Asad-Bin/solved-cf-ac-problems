c4de5f35d1dceae7444f9e8e9006ccbfbc957ecd
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-06-28 20:37:00 +0600

    Add solution for problem `A - XOR Mixup` Link: https://codeforces.com/contest/1698/problem/A Tags: bitmasks, brute force, *800 Ref: https://codeforces.com/contest/1698/submission/162056735

---
+++ codeforces/1698/A.cpp
@@ -0,0 +1,75 @@
+// . . . Bismillahir Rahmanir Rahim . . . //
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+	int t, n, x;
+	
+	cin >> t;
+	while(t--){
+		cin >> n;
+		
+		for(int K = 0; K < n; K++) cin >> x;
+		
+		cout << x << "\n";
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
