851ba2ae2dde18e02584e1008270079fee2bf103
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-11-20 17:01:00 +0600

    Add solution for problem `E - Rock, Paper, Scissors` Link: https://codeforces.com/contest/1426/problem/E Tags: brute force, constructive algorithms, flows, greedy, math, *1800 Ref: https://codeforces.com/contest/1426/submission/181727030

---
+++ codeforces/1426/E.cpp
@@ -0,0 +1,73 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#ifndef ONLINE_JUDGE
+#define dbg_out cout
+#define debug(...) dbg_out << "DBG )> "; __f(#__VA_ARGS__, __VA_ARGS__);
+template<typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2> pr) { out << "{ " << pr.first << ", " << pr.second << " }"; return out; }
+template<typename T1> ostream& operator<<(ostream& out, vector<T1> vec) { out << "{ "; for (auto &x: vec) out << x << ", "; out << "}"; return out; }
+template<typename T1, size_t size> ostream& operator<<(ostream& out, array<T1, size> arr) { out << "{ "; for (auto &x: arr) out << x << ", "; out << "}"; return out; }
+template<typename T1, typename T2> ostream& operator<<(ostream& out, map<T1, T2> mp) { out << "{ ";for (auto &x: mp) out << x.first << ": " << x.second <<  ", "; out << "}"; return out; }
+template <typename Arg1> void __f(const char* name, Arg1&& arg1) { while (isspace(name[0])) name++; (isalpha(name[0]) || name[0] == '_') ? dbg_out << name << ": " << arg1 << "\n" : dbg_out << arg1 << "\n"; dbg_out.flush();}
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) { const char *comma = strchr(names + 1, ','); while (isspace(names[0])) names++; (isalpha(names[0]) || names[0] == '_') ? dbg_out.write(names, comma - names) << ": " << arg1 << " | " : dbg_out << arg1 << " | "; __f(comma + 1, args...);}
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n; cin >> n;
+	int a[3], b[3];
+	
+	for(int K = 0; K < 3; K++) cin >> a[K];
+	for(int K = 0; K < 3; K++) cin >> b[K];
+	
+	cout << max(0, a[0]-b[0]-b[2])+max(0, a[1]-b[1]-b[0])+max(0, a[2]-b[2]-b[1]) << ' ' << min(a[0], b[1])+min(a[1], b[2])+min(a[2], b[0]) << "\n";
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
