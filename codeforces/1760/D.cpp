0841f1ae555f8c44c2dbd217c814716f555e698f
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-11-21 20:54:00 +0600

    Add solution for problem `D - Challenging Valleys` Link: https://codeforces.com/contest/1760/problem/D Tags: implementation, two pointers, *1000 Ref: https://codeforces.com/contest/1760/submission/181934043

---
+++ codeforces/1760/D.cpp
@@ -0,0 +1,85 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#ifndef ONLINE_JUDGE
+#define dbg_out cout
+#define debug(...) dbg_out << "DBG )> "; __f(#__VA_ARGS__, __VA_ARGS__);
+template<typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2> pr) { out << "{ " << pr.first << ", " << pr.second << " }"; return out; }
+template<typename T1> ostream& operator<<(ostream& out, vector<T1> vec) { out << "{ "; for (auto &x: vec) out << x << ", "; out << "}"; return out; }
+template<typename T1, size_t size> ostream& operator<<(ostream& out, array<T1, size> arr) { out << "{ "; for (auto &x: arr) out << x << ", "; out << "}"; return out; }
+template<typename T1, typename T2> ostream& operator<<(ostream& out, map<T1, T2> mp) { out << "{ ";for (auto &x: mp) out << x.first << ": " << x.second <<  ", "; out << "}"; return out; }
+template <typename Arg1> void __f(const char* name, Arg1&& arg1) { while (isspace(name[0])) name++; (isalpha(name[0]) || name[0] == '_') ? dbg_out << name << ": " << arg1 << "\n" : dbg_out << arg1 << "\n"; dbg_out.flush();}
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) { const char *comma = strchr(names + 1, ','); while (isspace(names[0])) names++; (isalpha(names[0]) || names[0] == '_') ? dbg_out.write(names, comma - names) << ": " << arg1 << " | " : dbg_out << arg1 << " | "; __f(comma + 1, args...);}
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int t; cin >> t;
+	while(t--){
+		int n; cin>> n;
+		
+		int ara[n];
+		for(int K = 0; K < n; K++) cin >> ara[K];
+		
+		int l = 0, r = 0, cnt  = 0;
+		while(r < n){
+			while(r < n-1 && ara[r] == ara[r+1]) r++;
+			
+			if((l == 0 || ara[l-1] > ara[l]) && (r == n-1 || ara[r] < ara[r+1])) cnt++;
+			
+			debug(vector<int>{l, r});
+			l = r = r+1;
+		}
+		
+		if(cnt == 1) cout << "YES\n";
+		else cout << "NO\n";
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
