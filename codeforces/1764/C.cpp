a701a3e65a718d961aeb9f7382490dd8e49f1458
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-11-26 20:41:00 +0600

    Add solution for problem `C - Doremy's City Construction` Link: https://codeforces.com/contest/1764/problem/C Tags: graphs, greedy, *1400 Ref: https://codeforces.com/contest/1764/submission/182670956

---
+++ codeforces/1764/C.cpp
@@ -0,0 +1,88 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#ifndef ONLINE_JUDGE
+#define dbg_out cout
+#define debug(...) dbg_out << "DBG )> "; __f(#__VA_ARGS__, __VA_ARGS__);
+template<typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2> pr) { out << "{ " << pr.first << ", " << pr.second << " }"; return out; }
+template<typename T1> ostream& operator<<(ostream& out, vector<T1> vec) { out << "{ "; for (auto &x: vec) out << x << ", "; out << "}"; return out; }
+template<typename T1, size_t size> ostream& operator<<(ostream& out, array<T1, size> arr) { out << "{ "; for (auto &x: arr) out << x << ", "; out << "}"; return out; }
+template<typename T1, typename T2> ostream& operator<<(ostream& out, map<T1, T2> mp) { out << "{ ";for (auto &x: mp) out << x.first << ": " << x.second <<  ", "; out << "}"; return out; }
+template <typename Arg1> void __f(const char* name, Arg1&& arg1) { while (isspace(name[0])) name++; (isalpha(name[0]) || name[0] == '_') ? dbg_out << name << ": " << arg1 << "\n" : dbg_out << arg1 << "\n"; dbg_out.flush();}
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) { const char *comma = strchr(names + 1, ','); while (isspace(names[0])) names++; (isalpha(names[0]) || names[0] == '_') ? dbg_out.write(names, comma - names) << ": " << arg1 << " | " : dbg_out << arg1 << " | "; __f(comma + 1, args...);}
+#else
+#define debug(...)
+#endif
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int t; cin  >> t;
+	while(t--){
+		ll n; cin >> n;
+		ll ara[n];
+		map<ll, ll > mp;
+		for(int K = 0; K < n; K++){
+			cin >> ara[K];
+			mp[ara[K]]++;
+		}
+		
+		sort(ara, ara+n);
+		
+		ll ans = n*(n-1)/2, sum = 0;
+		for(auto x:mp){
+			ans -= x.second*(x.second-1)/2;
+			if(max(sum, n-sum-x.second) * x.second < x.second/2) ans += x.second/2;
+			else ans -= min(sum, n-sum-x.second) * x.second;
+			
+			sum += x.second;
+		}
+		
+		cout << ans << "\n";
+	}
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
