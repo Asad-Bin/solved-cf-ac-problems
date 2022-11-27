e75ea6fdbe7081156705686a6b6ca88f78327cc2
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-11-27 16:47:00 +0600

    Add solution for problem `B - Broken Keyboard` Link: https://codeforces.com/contest/1765/problem/B Tags: greedy, *800 Ref: https://codeforces.com/contest/1765/submission/182766713

---
+++ codeforces/1765/B.cpp
@@ -0,0 +1,92 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#ifndef ONLINE_JUDGE
+#define dbg_out cout
+#define debug(...) dbg_out << "DBG )> "; __f(#__VA_ARGS__, __VA_ARGS__);
+template<typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2> pr) { out << "{ " << pr.first << ", " << pr.second << " }"; return out; }
+template<typename T1> ostream& operator<<(ostream& out, vector<T1> vec) { out << "{ "; for (auto &x: vec) out << x << ", "; out << "}"; return out; }
+template<typename T1, size_t size> ostream& operator<<(ostream& out, array<T1, size> arr) { out << "{ "; for (auto &x: arr) out << x << ", "; out << "}"; return out; }
+template<typename T1, typename T2> ostream& operator<<(ostream& out, map<T1, T2> mp) { out << "{ ";for (auto &x: mp) out << x.first << ": " << x.second <<  ", "; out << "}"; return out; }
+template <typename Arg1> void __f(const char* name, Arg1&& arg1) { while (isspace(name[0])) name++; (isalpha(name[0]) || name[0] == '_') ? dbg_out << name << ": " << arg1 << "\n" : dbg_out << arg1 << "\n"; dbg_out.flush();}
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) { const char *comma = strchr(names + 1, ','); while (isspace(names[0])) names++; (isalpha(names[0]) || names[0] == '_') ? dbg_out.write(names, comma - names) << ": " << arg1 << " | " : dbg_out << arg1 << " | "; __f(comma + 1, args...);}
+#else
+#define debug(...)
+#endif
+
+void solve(){
+	
+	ll n; cin>> n;
+	string s; cin>> s;
+	
+	s.push_back('#');
+	ll cnt = 0;
+	for(ll i= 0; i < n; i++){
+		if(cnt&1){
+			if(s[i] != s[i+1]){
+				
+				cout << "NO" << "\n";
+				return;
+			}
+			i++;
+			cnt++;
+		}else{
+			cnt++;
+		}
+	}
+	cout << "YES" << "\n";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	
+	int t; cin>> t;
+	
+	while(t--){
+		solve();
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
