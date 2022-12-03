105e4ca09d86da22c8eaa4edf53414de74014453
Asad Bin Saber <mdasad.binsaber@gmail.com>
2022-12-03 18:23:00 +0600

    Add solution for problem `D - Factorial and Multiple` Link: https://atcoder.jp/contests/abc280/tasks/abc280_d Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/abc280/submissions/36971420

---
+++ atcoder/abc280/D.cpp
@@ -0,0 +1,108 @@
+// . . . Bismillahir Rahmanir Rahim . . .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#ifndef ONLINE_JUDGE
+#define dbg_out cout
+#define debug(...) dbg_out << "DBG )> "; __f(#__VA_ARGS__, __VA_ARGS__);
+template<typename T1, typename T2> ostream& operator<<(ostream& out, pair<T1, T2> pr) { out << "{ " << pr.first << ", " << pr.second << " }"; return out; }
+template<typename T1> ostream& operator<<(ostream& out, vector<T1> vec) { out << "{ "; for (auto &x: vec) out << x << ", "; out << "}"; return out; }
+template<typename T1, size_t size> ostream& operator<<(ostream& out, array<T1, size> arr) { out << "{ "; for (auto &x: arr) out << x << ", "; out << "}"; return out; }
+template<typename T1, typename T2> ostream& operator<<(ostream& out, map<T1, T2> mp) { out << "{ ";for (auto &x: mp) out << x.first << ": " << x.second <<  ", "; out << "}"; return out; }
+template <typename Arg1> void __f(const char* name, Arg1&& arg1) { while (isspace(name[0])) name++; (isalpha(name[0]) || name[0] == '_') ? dbg_out << name << ": " << arg1 << "\n" : dbg_out << arg1 << "\n"; dbg_out.flush();}
+template <typename Arg1, typename... Args> void __f (const char* names, Arg1&& arg1, Args&&... args) { const char *comma = strchr(names + 1, ','); while (isspace(names[0])) names++; (isalpha(names[0]) || names[0] == '_') ? dbg_out.write(names, comma - names) << ": " << arg1 << " | " : dbg_out << arg1 << " | "; __f(comma + 1, args...);}
+#else
+#define debug(...)
+#endif
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	
+	ll k; cin >> k;
+	ll l = 1, r = k, ans = k;
+	
+	map<ll, ll> mp;
+	for(ll K = 2; K*K <= k; K++){
+		if(k % K == 0){
+			while(k%K==0){
+				k /= K;
+				mp[K]++;
+			}
+		}
+	}
+	if(k > 1) mp[k]++;
+	
+	
+	while(l <= r){
+		ll mid = (l + r)/2;
+		
+		bool ok = 1;
+		for(auto it:mp){
+			unsigned long long i = it.first, cnt = 0;
+			while(i <= (unsigned long long)mid){
+				cnt += mid/i;
+				i *= it.first;
+			}
+			if(cnt < (unsigned long long)it.second){ ok = 0; break; }
+		}
+		
+		if(ok){
+			ans = mid;
+			r = mid-1;
+		}
+		else{
+			l = mid + 1;
+		}
+	}
+	
+	cout << ans << "\n";
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
